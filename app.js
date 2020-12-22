552f292cef2d00f4044262f8cd94f5cc38a2aa82
Pedro Matos <matos@scubic.pt>
2020-12-22 13:28:51 +0000

    ditching flask for npm

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const entrypoint = __dirname + '/index.html'
+const app = express()
+const port = 5000
+
+app.get('/', (req, res) => {
+  res.sendFile(entrypoint)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
