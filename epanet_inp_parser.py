e4741219ec4e56dcfa64cf9c92d8e7e514793d8b
pdMa2s <pedroguilhermematos@ua.pt>
2021-01-08 17:34:48 +0000

    adding the node coordinates to the network json

--- epanet_inp_parser.py
+++ epanet_inp_parser.py
@@ -3,10 +3,12 @@ import wntr
 
 
 class JsonNode(dict):
-    def __init__(self, id_, type_):
+    def __init__(self, id_, type_, x, y):
         super().__init__()
         self.__setitem__("id", id_)
         self.__setitem__("type", type_)
+        self.__setitem__("x", x)
+        self.__setitem__("y", y)
 
 
 class JsonLink(dict):
@@ -25,9 +27,14 @@ def get_network_nodes(network_):
     node_registry = network_.nodes()
     json_nodes_ = []
     for node_id, node in node_registry:
-        jsn_n = JsonNode(node_id, node.node_type)
+        x, y = node.coordinates
+        jsn_n = JsonNode(node_id, node.node_type, x, y)
         json_nodes_.append(jsn_n)
     return json_nodes_
+
+
+def get_midway_point(coords_1, coords_2):
+    return coords_1[0] - coords_2[0], coords_1[1] - coords_2[1]
 
 
 def get_network_links(network_):
@@ -35,14 +42,16 @@ def get_network_links(network_):
     json_links_ = []
     json_nodes_ = []
     for link_id, link in link_registry:
-        start_node_id = link.start_node.name
-        end_node_id = link.end_node.name
+        start_node = link.start_node
+        end_node = link.end_node
         if link.link_type in ("Pump", "Valve"):
-            json_nodes_.append(JsonNode(link_id, link.link_type))
-            json_links_.append(JsonLink(start_node_id, link_id))
-            json_links_.append(JsonLink(link_id, end_node_id))
+            start_coordinates = start_node.coordinates
+            end_coordinates = end_node.coordinates
+            json_nodes_.append(JsonNode(link_id, link.link_type, *get_midway_point(start_coordinates, end_coordinates)))
+            json_links_.append(JsonLink(start_node.name, link_id))
+            json_links_.append(JsonLink(link_id, end_node.name))
         else:
-            jsn_l = JsonLink(start_node_id, end_node_id)
+            jsn_l = JsonLink(start_node.name, end_node.name)
             json_links_.append(jsn_l)
     return json_links_, json_nodes_
 
